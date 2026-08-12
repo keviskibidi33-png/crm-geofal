f862f56ca5713979f39c07dcc0ec06a4f85d50a1
keviskibidi33-png <keviskibidi33@gmail.com>
2026-08-12 11:22:58 -0500

    revert(cotizacion): restore tailored native cotizacion module from commit 233d18f

--- src/components/dashboard/cotizacion/index.ts
+++
@@ -1,4 +0,0 @@
-export { CotizadoraModule as default, CotizadoraModule } from "./cotizadora-module"
-export * from "./types"
-export { CreateQuoteDialog } from "./create-quote-dialog"
-export { QuotePreviewPanel } from "./quote-preview-panel"
